6e5cb6f04817f86d5cb3b4a3e552b12e72607ba4
gmaffy <mafireyi@gmail.com>
2025-06-04 21:49:45 +0200

    Update alignShortReads.go

--- alignment/alignShortReads.go
+++ alignment/alignShortReads.go
@@ -18,6 +18,9 @@ func AlignShortReadsMem(referencePath string, forwardPath string, reversePath st
 	}
 	readGroup := fmt.Sprintf("@RG\\tID:%s.1\\tSM:%s\\tLB:%s\\tPL:BGISEQ", sampleName, sampleName, libName)
 	sortedBam := fmt.Sprintf("%s/%s.sorted.bam", lineDir, sampleName)
+	rgmdBam := fmt.Sprintf("%s/%s.RGMD.bam", lineDir, sampleName)
+	rgmdMetrics := fmt.Sprintf("%s/%s.RGMD.metrics.txt", lineDir, sampleName)
+	rgmdIndex := fmt.Sprintf("%s/%s.RGMD.bai", lineDir, sampleName)
 
 	// Construct the one-liner shell command
 	cmdStr := fmt.Sprintf(`bwa mem -t 8 -M -Y -R '%s' %s %s %s | samtools sort -o %s`, readGroup, referencePath, forwardPath, reversePath, sortedBam)
@@ -28,6 +31,27 @@ func AlignShortReadsMem(referencePath string, forwardPath string, reversePath st
 
 	err := cmd.Run()
 	if err != nil {
+		return
+	}
+	fmt.Printf("Marking duplicates ....")
+	mDupCmdStr := fmt.Sprintf(`gatk --java-options "-Xmx8G" MarkDuplicates -I %s -O %s -M %s`, sortedBam, rgmdBam, rgmdMetrics)
+	mDupCmd := exec.Command("bash", "-c", mDupCmdStr)
+	mDupCmd.Stdout = os.Stdout
+	mDupCmd.Stderr = os.Stderr
+
+	mErr := mDupCmd.Run()
+	if mErr != nil {
+		return
+	}
+
+	fmt.Printf("Index Bam ....")
+	indexCmdStr := fmt.Sprintf(`gatk --java-options "-Xmx8G" BuildBamIndex -I %s -O %s`, rgmdBam, rgmdIndex)
+	indexCmd := exec.Command("bash", "-c", indexCmdStr)
+	indexCmd.Stdout = os.Stdout
+	indexCmd.Stderr = os.Stderr
+
+	iErr := indexCmd.Run()
+	if iErr != nil {
 		return
 	}
 
