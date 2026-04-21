5b944da986746ef6e0a1cbc1b0b6584d8d516eee
Godwin Mafireyi <42168937+godwinmafireyi@users.noreply.github.com>
2026-04-21 16:34:47 +0200

    hd

--- alignment/helper.go
+++ alignment/helper.go
@@ -12,6 +12,7 @@ import (
 func BamToCram(bamPath, refFasta string, verbose bool) error {
 	cramPath := strings.TrimSuffix(bamPath, filepath.Ext(bamPath)) + ".cram"
 	bamToCramStr := fmt.Sprintf(`samtools view -T %s -C -o %s %s`, refFasta, cramPath, bamPath)
+	fmt.Printf("\n-------------------------------------------------------------------\nRunning: %s ...\n------------------------------------------------------------------\n\n", bamToCramStr)
 	var err error
 	if verbose {
 		err = utils.RunBashCmdVerbose(bamToCramStr)
@@ -25,7 +26,7 @@ func BwaMem2Align(forwardPath string, reversePath string, referencePath string, 
 
 	readGroup := fmt.Sprintf("@RG\\tID:%s.1\\tSM:%s\\tLB:%s\\tPL:BGISEQ", sampleName, sampleName, libName)
 	cmdStr := fmt.Sprintf(`bwa-mem2 mem -t %v -M -Y -R '%s' %s %s %s | samtools sort -o %s`, threads, readGroup, referencePath, forwardPath, reversePath, sortedBam)
-	fmt.Printf("%s\n--------------------------------------------\n\n", cmdStr)
+	fmt.Printf("\n-------------------------------------------------------------------\nRunning: %s ...\n------------------------------------------------------------------\n\n", cmdStr)
 
 	var err error
 	if verbose {
@@ -39,7 +40,7 @@ func BwaMem2Align(forwardPath string, reversePath string, referencePath string, 
 func BwaMemAlign(forwardPath string, reversePath string, referencePath string, sampleName string, libName string, threads int, sortedBam string, verbose bool) error {
 	readGroup := fmt.Sprintf("@RG\\tID:%s.1\\tSM:%s\\tLB:%s\\tPL:BGISEQ", sampleName, sampleName, libName)
 	cmdStr := fmt.Sprintf(`bwa mem -t %v -M -Y -R '%s' %s %s %s | samtools sort -o %s`, threads, readGroup, referencePath, forwardPath, reversePath, sortedBam)
-	fmt.Printf("%s\n--------------------------------------------\n\n", cmdStr)
+	fmt.Printf("\n-------------------------------------------------------------------\nRunning: %s ...\n------------------------------------------------------------------\n\n", cmdStr)
 
 	var err error
 	if verbose {
@@ -52,7 +53,7 @@ func BwaMemAlign(forwardPath string, reversePath string, referencePath string, s
 
 func Bowtie2Align(forwardPath string, reversePath string, referencePath string, sortedBam string, sampleName string, libName string, threads int, verbose bool) error {
 	cmdStr := fmt.Sprintf(`bowtie2 -I 0 -X 1000 -x %s -1 %s -2 %s --end-to-end --sensitive --threads %v  --rg-id %s.1 --rg PL:BGISEQ --rg SM:%s --rg LB:%s | samtools sort -o %s`, referencePath, forwardPath, reversePath, threads, sampleName, sampleName, libName, sortedBam)
-	fmt.Println(cmdStr)
+	fmt.Printf("\n-------------------------------------------------------------------\nRunning: %s ...\n------------------------------------------------------------------\n\n", cmdStr)
 	var err error
 	if verbose {
 		err = utils.RunBashCmdVerbose(cmdStr)
@@ -83,7 +84,7 @@ func Pbmm2Align(sePath string, referencePath string, sortedBam string, preset st
 	}
 
 	pbmm2CmdStr := fmt.Sprintf(`pbmm2 align --sort -j %v --preset %s %s %s %s`, threads, preset, referencePath+".mmi", sePath, sortedBam)
-	fmt.Println(pbmm2CmdStr)
+	fmt.Printf("\n-------------------------------------------------------------------\nRunning: %s\n------------------------------------------------------------------\n\n", pbmm2CmdStr)
 	var pbmm2Err error
 	if verbose {
 		pbmm2Err = utils.RunBashCmdVerbose(pbmm2CmdStr)
@@ -96,7 +97,7 @@ func Pbmm2Align(sePath string, referencePath string, sortedBam string, preset st
 
 func ReadGroups(sortedBam string, rgBam string, sampleName string, libName string, verbose bool) error {
 	rgCmdStr := fmt.Sprintf(`gatk AddOrReplaceReadGroups -I %s -O %s -ID %s.1 -LB %s -PL PACBIO -PU BKD -SM %s`, sortedBam, rgBam, sampleName, libName, sampleName)
-	fmt.Printf("%s\n-----------------------------------------------\n\n", rgCmdStr)
+	fmt.Printf("\n-------------------------------------------------------------------\nRunning: %s\n------------------------------------------------------------------\n\n", rgCmdStr)
 	var rgErr error
 	if verbose {
 		rgErr = utils.RunBashCmdVerbose(rgCmdStr)
@@ -108,7 +109,7 @@ func ReadGroups(sortedBam string, rgBam string, sampleName string, libName strin
 
 func BamIndex(bam string, verbose bool) error {
 	indexCmdStr := fmt.Sprintf(`samtools index %s`, bam)
-	fmt.Printf("%s\n-----------------------------------------------\n\n", indexCmdStr)
+	fmt.Printf("\n-------------------------------------------------------------------\nRunning: %s\n------------------------------------------------------------------\n\n", indexCmdStr)
 
 	var err error
 	if verbose {
@@ -131,21 +132,14 @@ func MarkDuplicates(referencePath string, sortedBam string, verbose bool, aligne
 	rgmdMetrics := baseName + ".RGMD.metrics.txt"
 
 	var cmd string
-	switch aligner {
-	case "bwa-mem":
-		cmd = fmt.Sprintf(`gatk --java-options "-Xmx8G" MarkDuplicates -R %s -I %s -O %s -M %s --VERBOSITY %s`, referencePath, sortedBam, rgmdBam, rgmdMetrics, gatkLogLevel)
-
-	case "bwa-mem2":
-		cmd = fmt.Sprintf(`gatk --java-options "-Xmx8G" MarkDuplicates -R %s -I %s -O %s -M %s --VERBOSITY %s`, referencePath, sortedBam, rgmdBam, rgmdMetrics, gatkLogLevel)
-
-	case "bowtie2":
-		cmd = fmt.Sprintf(`gatk --java-options "-Xmx8G" MarkDuplicates -R %s -I %s -O %s -M %s --VERBOSITY %s`, referencePath, sortedBam, rgmdBam, rgmdMetrics, gatkLogLevel)
-
-	case "pbmm2":
+	if aligner == "pbmm2" {
 		cmd = fmt.Sprintf(`pbmm2 markdup %s %s`, sortedBam, rgmdBam)
+	} else {
+		cmd = fmt.Sprintf(`gatk --java-options "-Xmx8G" MarkDuplicates -R %s -I %s -O %s -M %s --VERBOSITY %s`, referencePath, sortedBam, rgmdBam, rgmdMetrics, gatkLogLevel)
+
 	}
 
-	fmt.Printf("%s\n-----------------------------------------------\n\n", cmd)
+	fmt.Printf("\n-------------------------------------------------------------------\nRunning: %s\n------------------------------------------------------------------\n\n", cmd)
 
 	var err error
 	if verbose {
